30cc4e78383b2b779e7326134e5584a433d9e116
Patel-Phaneendra <99329272+Patel-Phaneendra@users.noreply.github.com>
2022-07-22 16:28:09 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+export IMAGE_NAME=shipimg/micro41
+if [ "$IS_PULL_REQUEST" != true ]; then
+  sudo docker build -t $IMAGE_NAME:$BRANCH.$SHIPPABLE_BUILD_NUMBER .
+  sudo docker push $IMAGE_NAME:$BRANCH.$SHIPPABLE_BUILD_NUMBER
+else
+  echo "skipping because it's a PR"
+fi
